89d6aca79f4ebb2ee87d7311180f7a9fea31562d
unknown <rohitgurung086@gmail.com>
2024-12-18 15:12:56 +0530

    form page added

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -4,7 +4,24 @@ import { assets } from '../../../public/assets/assets'
 import Image from 'next/image'
 
 const AddProduct = () => {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState(
+    {
+      title: "",
+      description:"",
+      category:"startup",
+      author:"John Doe",
+      authorImg: "./author_img.png"
+    }
+  )
+
+  const onChangeHandler = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data, [name]: value}))
+    console.log(data);
+  }
+
 
   // Handle image selection
   const handleImageChange = (e) => {
@@ -13,12 +30,16 @@ const AddProduct = () => {
     }
   };
 
+  //Submit data logic
+  const onSubmitHandler = (e) =>{
+    // e.preventDefault()
+  }
+
   return (
     <>
-      <form className='mt-3 px-5 sm:pt-2 sm:pl-8'>
-        <p className='text-base'>Upload thumbnail</p>
+      <form onSubmit={onSubmitHandler} className='px-5 sm:pt-2 sm:pl-8'>
 
-        <label htmlFor="image" className='mt-4  cursor-pointer sm:w-[50px]'>
+        <label htmlFor="image" className='mt-3  cursor-pointer sm:w-[50px]'>
           {/* If no image is uploaded, show the default upload area */}
           {image ? (
             <img
@@ -41,27 +62,27 @@ const AddProduct = () => {
 
         {/* File input */}
         <input
-          onChange={handleImageChange}
           type='file'
           id='image'
-          hidden
+          name="authorImg"
+          className='mt-3'
           required
         />
         <p className='text-base mt-4'>Blog required</p>
-        <input type="text" placeholder='Enter text here'
+        <input type="text" name='title' onChange={onChangeHandler} value={data.title} placeholder='Enter text here'
           className='w-full sm:w-[500px] mt-3 px-4 py-1 border' />
         <p className='text-base mt-4'>Blog Description required</p>
-        <textarea type="text" rows={3} placeholder='Enter the description here'
+        <textarea name='description' onChange={onChangeHandler} value={data.description} type="text" rows={3} placeholder='Enter the description here'
           className='w-full sm:w-[500px] mt-3 px-4 py-2 border' />
 
         <p className='text-base mt-3'>Blog Category</p>
-        <select name="category" id="" className='w-40 mt-3 px-4 py-2 border text-gray-500'>
+        <select name="category" onChange={onChangeHandler} value={data.category}  id="" className='w-40 mt-3 px-4 py-2 border text-gray-500'>
           <option value="start up">Start up</option>
           <option value="technology">Technology</option>
           <option value="lifestyle">Life Style</option>
         </select>
         <br />
-        <button type='submit' className='mt-6 w-36 h-10 border-radius bg-black text-white'>Submit</button>
+        <button type='submit' className='mt-6 w-36 h-10 border-radius bg-black text-white'>Add</button>
       </form>
     </>
   )
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -11,7 +11,7 @@ const layout = ({ children }) => {
                 <div className='flex flex-col w-full'>
                     <div className='flex items-center justify-between w-full py-2
                      border-b border-black'>
-                        <h3 className='font-medium ps-8 text-base font-bold'>Admin Panel</h3>
+                        <h3 className='font-medium ps-8 text-base'>Admin Panel</h3>
                         <Image src={assets.profile_icon} width={40} alt='profile_icon' />
                     </div>
                     {children}
