be945badbe8988df901253e00ee832caa222124b
unknown <rohitgurung086@gmail.com>
2024-12-26 13:07:09 +0530

    Blog List page created

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+"use client"
+import BlogTableItem from '@/components/AdminComponents/BlogTableItem'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const blogList = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    try {
+      const response = await axios.get('/api/blog');
+      console.log(response.data); // Log the response to see its structure
+      setBlogs(response.data.blogs);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [])
+
   return (
-    <div>blogList</div>
+    <div className="flex-1 pt-2 px-2 sm:pt-12 sm:pl-10">
+      <h1>All Blogs</h1>
+      <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-2 border border-gray-400 scrollbar-hide">
+        {blogs.length === 0 ? (
+          <p>No blogs found</p>
+        ) : (
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-sm text-gray-700 text-left uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="hidden sm:block px-6 py-3">
+                  Author Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Blog Title
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((item, index) => (
+                <BlogTableItem
+                  key={index}
+                  mongoId={item._id}
+                  title={item.title}
+                  author={item.author}
+                  authorImg={item.authorImg}
+                  date={item.date}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+import { assets } from '../../../public/assets/assets';
+
+const BlogTableItem = ({ authorImg, title, author, date }) => {
+    const blogDate = new Date(date)
+    return (
+        <tr className="bg-white border-b">
+            <th scope="row" className="items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <Image
+                    src={authorImg ? authorImg : assets.profile_icon}
+                    alt={author ? author : 'No Author'}
+                    width={40}
+                    height={40}
+                />
+                <p>{author ? author : "No Author"}</p>
+            </th>
+            <td className="px-6 py-4">{title ? title : "No Title"}</td>
+            <td className="px-6 py-4">{blogDate.toDateString()}</td>
+            <td className="px-6 py-4">X</td>
+        </tr>
+    )
+}
+
+export default BlogTableItem
